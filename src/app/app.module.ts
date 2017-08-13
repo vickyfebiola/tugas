be9196f42fb43d2276825acb00740e2ee23d25b1
vickyfebiola <vickyfebiola@gmail.com>
2017-08-13 21:14:19 +0700

    pesan penandamu

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { Storage } from '@ionic/storage';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { NewPlacePage } from '../pages/new-place/new-place';
+import { PlacesService } from '../services/places.service';
+import { IonicStorageModule } from '@ionic/storage';
+import { Geolocation } from '@ionic-native/geolocation'; 
+import { PlacePage } from '../pages/place/place';
+import { AgmCoreModule } from '@agm/core';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    NewPlacePage,
+    PlacePage
+
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot(),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyC5kTKWGG2EiAv7NJiUhNwJfgysOhhnCXw '    
+    })
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    NewPlacePage,
+    PlacePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}, PlacesService, Geolocation]
+})
+export class AppModule {}
